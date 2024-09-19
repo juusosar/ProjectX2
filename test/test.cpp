40725339e453a538e94f394d1ce3c48dc3a66094
juuso-Arch <juuso.sarkela@outlook.com>
2024-09-19 21:56:27 +0300

    Update ProjectX2 to be ready for CI

--- test/test.cpp
+++ test/test.cpp
@@ -11,5 +11,13 @@ FUZZ_TEST_SETUP() {
 }
 
 FUZZ_TEST(const uint8_t *data, size_t size) {
-  /*Your code here*/
+  /  FuzzedDataProvider fuzzed_data(data, size); 
+  float num_1 = fuzzed_data.ConsumeFloatingPoint<float>();
+  float num_2 = fuzzed_data.ConsumeFloatingPoint<float>();
+  std::string op = fuzzed_data.ConsumeBytesAsString(1);
+
+  char char_op = op[0];
+
+  calculator(num_1, char_op, num_2);
+
 }
